64b9b958240ab17d48c7b6f6a190fb245aecf871
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-12-20 14:13:17 -0800

    New single output script, new output location

--- posts/2018/pre_rsb/do_graphing.sh
+++ posts/2018/pre_rsb/do_graphing.sh
@@ -6,5 +6,8 @@ set -x
 MIN_PCT=1
 MAX_PCT=99
 
-PLATFORM=mac TEST_NAME=rails_10k erb single_csv_set.html.erb > mac_rails_10k.html
+PLATFORM=mac TEST_NAME=rails_10k erb single_csv_set.html.erb > output/mac_rails_10k.html
+PLATFORM=mac TEST_NAME=rack_10k erb single_csv_set.html.erb > output/mac_rack_10k.html
 
+PLATFORM=ubuntu TEST_NAME=rails_10k erb single_csv_set.html.erb > output/ubuntu_rails_10k.html
+PLATFORM=ubuntu TEST_NAME=rails_1mil erb single_csv_set.html.erb > output/ubuntu_rails_1mil.html
