5cb3fc416b903cd18fdcb6c5b74acef977f72d49
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-12-20 14:15:59 -0800

    Check in new dataset script changes with output

--- posts/2018/pre_rsb/do_graphing.sh
+++ posts/2018/pre_rsb/do_graphing.sh
@@ -10,4 +10,8 @@ PLATFORM=mac TEST_NAME=rails_10k erb single_csv_set.html.erb > output/mac_rails_
 PLATFORM=mac TEST_NAME=rack_10k erb single_csv_set.html.erb > output/mac_rack_10k.html
 
 PLATFORM=ubuntu TEST_NAME=rails_10k erb single_csv_set.html.erb > output/ubuntu_rails_10k.html
+PLATFORM=ubuntu TEST_NAME=rails_10k_2 erb single_csv_set.html.erb > output/ubuntu_rails_10k_2.html
 PLATFORM=ubuntu TEST_NAME=rails_1mil erb single_csv_set.html.erb > output/ubuntu_rails_1mil.html
+
+PLATFORM=ubuntu TEST_NAME=rack_10k erb single_csv_set.html.erb > output/ubuntu_rack_10k.html
+PLATFORM=ubuntu TEST_NAME=rack_100k erb single_csv_set.html.erb > output/ubuntu_rack_100k.html
